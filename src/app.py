56e919bc6116bb37f7ec2c479060175a96877b93
Fernando Monnot <fermonnot@gmail.com>
2022-10-20 17:16:14 -0400

    todo-crud :sparkles:

--- src/app.py
+++ src/app.py
@@ -2,41 +2,28 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-humans = []
+todos = [{ "label": "Primera Tarea", "done": False }]
 
 #decoradores 
-@app.route('/')
-def hello_world():
-    return '<h1>Welcome</h1>'
+@app.route("/todos", methods = ['GET'])
+def get_todos():
+    json_text = jsonify(todos)
+    print('this are the pending task')
+    return json_text
 
+@app.route("/todos", methods = ['POST'])
+def add_todos():
+    request_body = request.get_json(force=True)
+    print('New task added', request_body)
+    todos.append(request_body)
+    return jsonify(todos)
 
-@app.route('/humans', methods=['GET'])
-@app.route('/humans/<int:user_id>', methods=['GET'])
-def handle_humans(user_id = None):
-    if request.method == 'GET':
-        if user_id is None:
-            return humans , 200
-        else:
-            human = list(filter(lambda item: item["id"] == user_id, humans))
-            return human, 200
-    return humans, 405
-
-
-@app.route('/humans', methods=['POST'])
-def add_new_human():
-    if request.method == 'POST':
-        body = request.json
-        if  body.get("name") is None:
-            return {"message":"error propertie bad "} ,400
-        if body.get("lastname") is None:
-            return jsonify({"message":"error propertie bad "} ,400)
-        
-        body.update({"id": len(humans)+1})
-
-        humans.append(body)
-        return humans, 201
+@app.route("/todos/<int:index>", methods = ["DELETE"])
+def delete_todos(index):
+    print('task eliminated', index)
+    todos.pop(index)
+    return jsonify(todos)
 
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0', port="8000", debug=True)
-# app.run(host="0.0.0.0", debug=True)
+    app.run(host='127.0.0.1', port="8000", debug=True)
